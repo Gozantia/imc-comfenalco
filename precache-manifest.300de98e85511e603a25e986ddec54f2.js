e06a5b49a5f29e1901fc386b08624ea61d4f6cec
Gozantia <lordzantiago@gmail.com>
2020-08-27 12:56:37 -0500

    Updates

---
+++ precache-manifest.300de98e85511e603a25e986ddec54f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ffd478fdf930648d6ca7ae678b95bd5",
+    "url": "/imc-comfenalco/index.html"
+  },
+  {
+    "revision": "80142e55d98120bead81",
+    "url": "/imc-comfenalco/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e6c3aba8de11282b1382",
+    "url": "/imc-comfenalco/static/js/2.09d32d23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/imc-comfenalco/static/js/2.09d32d23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80142e55d98120bead81",
+    "url": "/imc-comfenalco/static/js/main.4e93dd39.chunk.js"
+  },
+  {
+    "revision": "cf5ef7bb299543477c49",
+    "url": "/imc-comfenalco/static/js/runtime-main.e4874b84.js"
+  }
+]);
